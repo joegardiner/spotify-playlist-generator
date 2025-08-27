f4aa49803ceee5527f6a321685bf5ccb63a2faa7
Joe Gardiner <joegardiner@live.co.uk>
2025-08-27 03:47:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ document.getElementById("fetchBtn").onclick = async () => {
     
     for (const artistName of artistNames) {
       // Search artist
+      document.getElementById("status").innerText = `Processing ${artistName}...`;
       let res = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(`artist:"${artistName}"`)}&type=artist&limit=5`, {
         headers: { Authorization: "Bearer " + accessToken }
       });
